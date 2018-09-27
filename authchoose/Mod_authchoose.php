8a4ae45abb1078167d3cd4ab6b6ab24878d361e3
git-marijus <mario@mariovavti.com>
2018-09-27 23:05:10 +0200

    files

---
+++ authchoose/Mod_authchoose.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Name: Auth Choose
+ * Description: Allow magic authentication only to websites of your immediate connections.
+ *
+ */
+
+namespace Zotlabs\Module;
+
+use App;
+use Zotlabs\Lib\Apps;
+use Zotlabs\Web\Controller;
+
+class Authchoose extends Controller {
+
+	function get() {
+
+		if(! local_channel())
+			return;
+
+		$desc = t('Allow magic authentication only to websites of your immediate connections');
+
+		if(! Apps::addon_app_installed(local_channel(), 'authchoose')) {
+			//Do not display any associated widgets at this point
+			App::$pdl = '';
+
+			$o = '<b>Authchoose App (Not Installed):</b><br>';
+			$o .= $desc;
+			return $o;
+		}
+
+		$content = '<b>Authchoose App Installed:</b><br>';
+		$content .= $desc;
+
+		$tpl = get_markup_template("settings_addon.tpl");
+
+		$o .= replace_macros($tpl, array(
+			'$title' => t('Authchoose'),
+			'$content'  => $content
+		));
+
+		return $o;
+	}
+	
+}
