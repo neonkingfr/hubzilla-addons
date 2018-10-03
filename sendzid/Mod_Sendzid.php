f73c6a2f3fbff9216ad6fd85f22555190f531bcf
git-marijus <mario@mariovavti.com>
2018-10-03 11:41:45 +0200

    send ZID app

---
+++ sendzid/Mod_Sendzid.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Zotlabs\Module;
+
+use App;
+use Zotlabs\Lib\Apps;
+use Zotlabs\Web\Controller;
+
+class Sendzid extends Controller {
+	function get() {
+		if(! local_channel())
+			return;
+
+		$desc = t('Send your identity to all websites');
+
+		if(! Apps::addon_app_installed(local_channel(), 'sendzid')) {
+			//Do not display any associated widgets at this point
+			App::$pdl = '';
+
+			$o = '<b>Sendzid App (Not Installed):</b><br>';
+			$o .= $desc;
+			return $o;
+		}
+
+		$content = '<div class="section-content-info-wrapper">' . $desc . '</div>';
+
+		$tpl = get_markup_template("settings_addon.tpl");
+
+		$o .= replace_macros($tpl, array(
+			'$action_url' => '',
+			'$form_security_token' => '',
+			'$title' => t('Send ZID'),
+			'$content'  => $content,
+			'$baseurl'   => z_root(),
+			'$submit'    => ''
+		));
+
+		return $o;
+	}
+}
--- sendzid/sendzid.php
+++ sendzid/sendzid.php
@@ -9,32 +9,22 @@
  * Maintainer: none
  */
 
+use Zotlabs\Lib\Apps;
+use Zotlabs\Extend\Hook;
+use Zotlabs\Extend\Route;
+
 function sendzid_load() {
-	\Zotlabs\Extend\Hook::Register('get_features','addon/sendzid/sendzid.php','sendzid_get_features');
-	\Zotlabs\Extend\Hook::Register('zidify','addon/sendzid/sendzid.php','sendzid_zidify');
+	Hook::register('zidify','addon/sendzid/sendzid.php','sendzid_zidify');
+	Route::register('addon/sendzid/Mod_Sendzid.php','sendzid');
 }
 
 function sendzid_unload() {
-	\Zotlabs\Extend\Hook::unregister_by_file('addon/sendzid/sendzid.php');
-}
-
-function sendzid_get_features(&$x) {
-
-	$entry = [
-		'sendzid',                                                                         
-		t('Extended Identity Sharing'),                                                                      
-		t('Share your identity with all websites on the internet. When disabled, identity is only shared with $Projectname sites.'),
-		false,                                                                              
-		get_config('feature_lock','sendzid'),                                              
-		feature_level('sendzid',4),          
-	];
-
-	$x['features']['general'][] = $entry;
-
+	Hook::unregister_by_file('addon/sendzid/sendzid.php');
+	Route::unregister('addon/sendzid/Mod_Sendzid.php','sendzid');
 }
 
 function sendzid_zidify(&$x) {
-	if(local_channel() && feature_enabled(local_channel(),'sendzid')) {
+	if(local_channel() && Apps::addon_app_installed(local_channel(), 'sendzid')) {
 		$x['zid'] = true;
 	}
 }
