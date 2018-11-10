cf24ebc177e322d6a59e25556055b7e04e37add5
Max Kostikov <max@kostikov.co>
2018-11-10 20:29:36 +0100

    Update diaspora.php

--- diaspora/diaspora.php
+++ diaspora/diaspora.php
@@ -750,7 +750,7 @@ function diaspora_discover(&$b) {
 
 		$photos = import_xchan_photo($vcard['photo'],$addr);
 		$r = q("update xchan set xchan_photo_date = '%s', xchan_photo_l = '%s', xchan_photo_m = '%s', xchan_photo_s = '%s', xchan_photo_mimetype = '%s' where xchan_hash = '%s'",
-			dbescdate(datetime_convert()),
+			dbesc(datetime_convert()),
 			dbesc($photos[0]),
 			dbesc($photos[1]),
 			dbesc($photos[2]),
