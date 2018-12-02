dad7202b5e1b13b685c2e3056bb919234e11abdb
Max Kostikov <max@kostikov.co>
2018-12-02 15:13:17 +0100

    Enable / disable cache by App load / unload

--- photocache/Mod_Photocache.php
+++ photocache/Mod_Photocache.php
@@ -7,7 +7,7 @@ use Zotlabs\Lib\Apps;
 use Zotlabs\Web\Controller;
 
 class Photocache extends Controller {
-	
+
 	function post() {
 			
 		if(! local_channel())
@@ -17,39 +17,11 @@ class Photocache extends Controller {
 			return;
 		
 		check_form_security_token_redirectOnErr('photocache', 'photocache');
-
-		$switch = intval($_POST['cache_enable']);
-		
-		if(! $switch) {
-			$r = q("SELECT xchan, content, COUNT(*) AS qty FROM photo WHERE xchan IN 
-				(SELECT DISTINCT xchan FROM photo WHERE photo_usage = %d AND uid = %d) 
-				GROUP BY xchan, content",
-				intval(PHOTO_CACHE),
-				intval(local_channel())
-			);
-			if($r) {
-				foreach($r as $rr) {
-					$file = dbunescbin($rr['content']);
-					if($rr['qty'] == 1 && $file) {
-						if(is_file($file)) {
-							@unlink($file);
-							logger('deleted cached photo file ' . $file, LOGGER_DEBUG);
-						}
-					}
-				}
-			}
-	
-			$r = q("DELETE FROM photo WHERE photo_usage = %d AND uid = %d",
-				intval(PHOTO_CACHE),
-				intval(local_channel())
-			);
-		}
 		
 		$minres = intval($_POST['cache_minres']);
 		if($minres > 1024)
 			$minres = 1024;
 
-		set_pconfig(local_channel(), 'photocache', 'cache_enable', $switch);
 		set_pconfig(local_channel(), 'photocache', 'cache_minres', $minres);
 
 		info(t('Photo Cache settings saved.') . EOL);
@@ -75,29 +47,14 @@ class Photocache extends Controller {
 		
 		$sc = '<div class="section-content-info-wrapper">' . $desc . '</div><br>';
 		
-		$switch = get_pconfig(local_channel(),'photocache','cache_enable', 0);
-
-		$sc .= replace_macros(get_markup_template('field_checkbox.tpl'), array(
+		$sc .= replace_macros(get_markup_template('field_input.tpl'), array(
 			'$field' => array(
-				'cache_enable', 
-				t('Enable Photo Cache for this channel'), 
-				$switch, 
-				t('Current cache policy is defined by hub adminstrator.'), 
-				array(t('No'),t('Yes'))
+				'cache_minres', 
+				t('Minimal photo size for caching'), 
+				get_pconfig(local_channel(),'photocache','cache_minres', 0), 
+				t('In pixels. 0 will be replaced with system default, from 1 up to 1024 (large images will be scaled to this value).')
 			),
-		));
-		
-		if($switch != 0) {
-
-			$sc .= replace_macros(get_markup_template('field_input.tpl'), array(
-				'$field' => array(
-					'cache_minres', 
-					t('Minimal photo size for caching'), 
-					get_pconfig(local_channel(),'photocache','cache_minres', 0), 
-					t('In pixels. 0 will be replaced with system default, from 1 up to 1024 (large images will be scaled to this value).')
-				),
-			));	
-		}
+		));	
 
 		$tpl = get_markup_template("settings_addon.tpl");
 
