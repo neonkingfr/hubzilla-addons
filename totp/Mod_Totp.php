dfdd3ba5191adb8af289fe388da718deac4576d6
Pete Yadlowsky <pmy@linux.com>
2018-12-05 21:32:19 -0500

    disambiguate namespace for TOTP object class

--- totp/Mod_Totp.php
+++ totp/Mod_Totp.php
@@ -32,7 +32,7 @@ class TOTPController extends \Zotlabs\Web\Controller {
 			}
 		if (isset($_POST['secret'])) {
 			require_once("library/totp.php");
-			$totp = new TOTP("channels.gnatter.org", "Gnatter Channels",
+			$totp = new \TOTP("channels.gnatter.org", "Gnatter Channels",
 					$account['account_email'], null, 30, 6);
 			$r = q("update account set account_2fa_secret='%s' where account_id=%d",
 					$totp->secret, $id);
