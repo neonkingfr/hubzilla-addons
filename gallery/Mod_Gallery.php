b547e0fe27a5889fc5d6be9460a3ad84fad3058b
Mario Vavti <mario@mariovavti.com>
2018-12-10 11:25:35 +0100

    Gallery addon version 0.2: adds ability to share albums, hooks into photo_view template for pretty fullsize photo view and adds attach_can_view_folder check

--- gallery/Mod_Gallery.php
+++ gallery/Mod_Gallery.php
@@ -5,6 +5,8 @@ namespace Zotlabs\Module;
 use App;
 use Zotlabs\Lib\Apps;
 
+require_once('include/attach.php');
+
 class Gallery extends \Zotlabs\Web\Controller {
 
 	function init() {
@@ -58,56 +60,70 @@ class Gallery extends \Zotlabs\Web\Controller {
 			return $o;
 		}
 
-		head_add_js('/addon/gallery/lib/photoswipe/dist/photoswipe.js');
-		head_add_js('/addon/gallery/lib/photoswipe/dist/photoswipe-ui-default.js');
-
-		head_add_css('/addon/gallery/lib/photoswipe/dist/photoswipe.css');
-		head_add_css('/addon/gallery/lib/photoswipe/dist/default-skin/default-skin.css');
-
 		nav_set_selected('Gallery');
 
+		$owner_uid = App::$data['channel']['channel_id'];
+		$sql_extra = permissions_sql($owner_uid, get_observer_hash(), 'attach');
+
+		$json_photo = '';
 		$json_album = '';
 
-		if(argc() > 2) {
-			$album_name = argv(2);
+		$photo = (($_GET['photo']) ? true : false);
+		if($photo) {
+			$photo_arr[0]['resource_id'] = $_GET['photo'];
+			$photo_arr[0]['src'] = z_root() . '/photo/' . $_GET['photo'] . '-0';
+			$photo_arr[0]['msrc'] = z_root() . '/photo/' . $_GET['photo'] . '-1';
+			$photo_arr[0]['w'] = $_GET['width'];
+			$photo_arr[0]['h'] = $_GET['height'];
+			$photo_arr[0]['title'] = $_GET['title'];
+			$json_photo = json_encode($photo_arr);
+		}
+		else {
+			if(argc() > 2) {
+				$album_name = argv(2);
 
-			for ($i = 3; $i < argc(); $i++) {
-			    $album_name .= '/' . argv($i);
+				for ($i = 3; $i < argc(); $i++) {
+				    $album_name .= '/' . argv($i);
+				}
+
+				$r = q("select hash from attach where is_dir = 1 and uid = %d and display_path = '%s' $sql_extra limit 1",
+					intval($owner_uid),
+					dbesc($album_name)
+				);
+
+				if($r) {
+					$arr['album_id'] = $r[0]['hash'];
+
+					$album_items = self::get_album_items($arr);
+					$json_album = json_encode($album_items);
+				}
 			}
 
-			$arr['album'] = $album_name;
+			$unsafe = ((array_key_exists('unsafe', $_GET) && $_GET['unsafe']) ? 1 : 0);
 
-			$album_items = self::get_album_items($arr);
-			$json_album = json_encode($album_items);
-		} 
-
-		$owner_uid = App::$data['channel']['channel_id'];
-		$sql_extra = permissions_sql($owner_uid, get_observer_hash(), 'photo');
-
-		$unsafe = ((array_key_exists('unsafe', $_GET) && $_GET['unsafe']) ? 1 : 0);
-
-		$albums = q("SELECT DISTINCT album FROM photo
-			WHERE uid = %d AND photo_usage = %d  
-			AND is_nsfw = %d $sql_extra 
-			ORDER BY album DESC",
-			intval($owner_uid),
-			intval(PHOTO_NORMAL),
-			intval($unsafe)
-		);
-
-		foreach($albums as $album) {
-			$r = q("SELECT album, resource_id, width, height 
-				FROM photo WHERE uid = %d AND photo_usage = %d  
-				AND is_nsfw = %d AND imgscale = 3 AND album = '%s' $sql_extra 
-				ORDER BY created DESC LIMIT 1",
-				intval($owner_uid),
-				intval(PHOTO_NORMAL),
-				intval($unsafe),
-				dbesc($album['album'])
-
+			$r = q("select display_path, hash from attach where is_dir = 1 and uid = %d $sql_extra order by display_path asc",
+				intval($owner_uid)
 			);
 
-			$items[] = $r[0];
+			$items = [];
+
+			if($r) {
+				foreach($r as $rr) {
+					if(attach_can_view_folder($owner_uid, get_observer_hash(), $rr['folder'])) {
+						$x = q("SELECT photo.album, attach.folder, photo.resource_id, photo.width, photo.height FROM photo
+							LEFT JOIN attach ON attach.folder = '%s' AND photo.resource_id = attach.hash WHERE
+							attach.uid = %d AND photo.photo_usage = %s AND photo.is_nsfw = %d AND photo.imgscale = 1 $sql_extra ORDER BY photo.created DESC LIMIT 1",
+							dbesc($rr['hash']),
+							intval($owner_uid),
+							intval(PHOTO_NORMAL),
+							intval($unsafe)
+						);
+						if($x) {
+							$items[] = [ 'album' => $x[0]['album'] ? $x[0]['album'] : '/', 'folder' => $x[0]['folder'], 'resource_id' => $x[0]['resource_id'], 'width' => $x[0]['width'], 'height' => $x[0]['height'] ];
+						}
+					}
+				}
+			}
 		}
 
 		$tpl = get_markup_template('gallery.tpl', 'addon/gallery');
@@ -116,8 +132,14 @@ class Gallery extends \Zotlabs\Web\Controller {
 			'$albums' => $items,
 			'$nick' => App::$data['channel']['channel_address'],
 			'$unsafe' => $unsafe,
-			'$json_album' => $json_album
+			'$json' => (($photo) ? $json_photo : $json_album),
+			'$aj' => $photo
 		]);
+
+		if($photo) {
+			echo $o;
+			killme();
+		}
 
 		return $o;
 	}
@@ -125,16 +147,19 @@ class Gallery extends \Zotlabs\Web\Controller {
 	function get_album_items($arr) {
 		$owner_uid = App::$data['channel']['channel_id'];
 
-		$sql_extra = permissions_sql($owner_uid, get_observer_hash(), 'photo');
+		if(! attach_can_view_folder($owner_uid, get_observer_hash(), $arr['album_id']))
+			return;
+
+		$sql_extra = permissions_sql($owner_uid, get_observer_hash(), 'attach');
 
 		$unsafe = ((array_key_exists('unsafe', $arr) && $arr['unsafe']) ? 1 : 0);
 
-		$r = q("SELECT resource_id, width, height, description, display_path 
-			FROM photo WHERE uid = %d AND album = '%s' AND photo_usage = %d  
-			AND is_nsfw = %d  AND imgscale = 1 $sql_extra 
-			ORDER by created DESC",
+		$r = q("SELECT p.resource_id, p.width, p.height, p.display_path, p.mimetype, p.imgscale, p.description, p.created FROM photo p INNER JOIN
+			(SELECT photo.resource_id, photo.imgscale FROM photo left join attach on attach.folder = '%s' and photo.resource_id = attach.hash WHERE attach.uid = %d AND photo.imgscale = 1 AND photo.photo_usage = %d AND photo.is_nsfw = %d $sql_extra GROUP BY photo.resource_id) ph 
+			ON (p.resource_id = ph.resource_id AND p.imgscale = ph.imgscale)
+			ORDER BY created DESC",
+			dbesc($arr['album_id']),
 			intval($owner_uid),
-			dbesc($arr['album']),
 			intval(PHOTO_NORMAL),
 			intval($unsafe)
 		);
@@ -156,4 +181,3 @@ class Gallery extends \Zotlabs\Web\Controller {
 	}
 
 }
-
