99cc1cdb1b64f5d003513723ea1cea567cb4394c
Pete Yadlowsky <pmy@linux.com>
2018-12-10 21:19:30 -0500

    drop totp_settings, totp_settings_post hooks
    and set /settings/totp route

--- totp/totp.php
+++ totp/totp.php
@@ -14,25 +14,21 @@ function totp_module(){};
 function totp_load() {
 	register_hook('construct_page', 'addon/totp/totp.php',
 		'totp_construct_page');
-	register_hook('feature_settings', 'addon/totp/totp.php',
-		'totp_settings');
-	register_hook('feature_settings_post', 'addon/totp/totp.php',
-		'totp_settings_post');
 	register_hook('logged_in', 'addon/totp/totp.php',
 		'totp_logged_in');
 	Zotlabs\Extend\Hook::register('module_loaded',
 		'addon/totp/totp.php','totp_module_loaded');
+	Zotlabs\Extend\Route::register('addon/totp/Settings/Totp.php',
+		'settings/totp');
 	}
 function totp_unload() {
 	unregister_hook('construct_page', 'addon/totp/totp.php',
 		'totp_construct_page');
-	unregister_hook('feature_settings', 'addon/totp/totp.php',
-		'totp_settings');
-	unregister_hook('feature_settings_post', 'addon/totp/totp.php',
-		'totp_settings_post');
 	unregister_hook('logged_in', 'addon/totp/totp.php',
 		'totp_logged_in');
 	Zotlabs\Extend\Hook::unregister_by_file('addon/totp/totp.php');
+	Zotlabs\Extend\Route::unregister('addon/totp/Settings/Totp.php',
+		'settings/totp');
 	}
 function totp_module_loaded(&$x) {
 	if ($x['module'] == 'totp') {
@@ -68,36 +64,4 @@ function totp_construct_page(&$a, &$b){
 		#htmlentities($some_setting) .
 		#"<pre>" . htmlentities(print_r(App::get_account(), true)) . "</pre>" .
 		'</div>';
-	}
-
-function totp_settings_post($a,$s) {
-	# possibly drop, as there's no settings submit event for this addon
-	if (!local_channel()) return;
-	}
-function totp_settings(&$a, &$s) {
-	$id = local_channel();
-	if (!$id) return;
-	$account = App::get_account();
-	if (!$account) return;
-	$acct_id = $account['account_id'];
-	require_once("addon/totp/class_totp.php");
-	$secret = $account['account_2fa_secret'];
-	$totp = new TOTP("channels.gnatter.org", "Gnatter Channels",
-			$account['account_email'], $secret == "" ? null : $secret, 30, 6);
-	$active_checked =
-		($account['account_2fa_active'] == 1
-			? "checked=\"checked\""
-			: "");
-	$sc = replace_macros(get_markup_template('settings.tpl','addon/totp'),
-			[
-			'$checked' => $active_checked,
-			'$secret' => $totp->secret,
-			'$qrcode_url' => "/totp/qrcode?s=",
-			'$salt' => microtime()
-			]);
-	$s .= replace_macros(get_markup_template('generic_addon_settings.tpl'),
-			array(
-				     '$addon' => array('',
-							 t('TOTP Settings'), '', ''),
-				     '$content'	=> $sc));
 	}
