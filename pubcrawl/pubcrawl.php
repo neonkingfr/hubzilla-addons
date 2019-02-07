cabbc0d0510a7e814e5647e11c9e74c5c228ef20
zotlabs <mike@macgirvin.com>
2019-02-07 00:13:52 -0800

    pubcrawl: regression from zot6 work

--- pubcrawl/pubcrawl.php
+++ pubcrawl/pubcrawl.php
@@ -870,11 +870,13 @@ function pubcrawl_item_mod_init($x) {
 
 		$sql_extra = item_permissions_sql(0);
 
-		$r = q("select * from item where mid like '%s' $item_normal $sql_extra limit 1",
+		$r = q("select * from item where (uuid = '%s' or mid like '%s') $item_normal $sql_extra limit 1",
+			dbesc($item_id),
 			dbesc($item_id . '%')
 		);
 		if(! $r) {
-			$r = q("select * from item where mid like '%s' $item_normal limit 1",
+			$r = q("select * from item where (uuid = '%s' or mid like '%s') $item_normal limit 1",
+				dbesc($item_id),
 				dbesc($item_id . '%')
 			);
 			if($r) {
