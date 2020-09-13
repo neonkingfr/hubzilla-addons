7c49a5626c5159985de0778829e99647fc637a6a
Max Kostikov <max@kostikov.co>
2020-09-13 11:56:33 +0200

    Store image modification date for xchan photo

--- pubcrawl/as.php
+++ pubcrawl/as.php
@@ -1271,7 +1271,7 @@ function as_actor_store($url,$person_obj) {
 
 	$photos = import_xchan_photo($icon,$url);
 	$r = q("update xchan set xchan_photo_date = '%s', xchan_photo_l = '%s', xchan_photo_m = '%s', xchan_photo_s = '%s', xchan_photo_mimetype = '%s' where xchan_hash = '%s'",
-		dbescdate(datetime_convert()),
+		dbescdate($photos[5]),
 		dbesc($photos[0]),
 		dbesc($photos[1]),
 		dbesc($photos[2]),
