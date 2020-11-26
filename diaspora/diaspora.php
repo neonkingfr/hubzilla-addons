e0ae9eca153244ec430bca4180b24516d3a4c68e
Mario <mario@mariovavti.com>
2020-11-26 14:59:00 +0000

    this should work now

--- diaspora/diaspora.php
+++ diaspora/diaspora.php
@@ -281,14 +281,6 @@ function diaspora_process_outbound(&$arr) {
 	logger('upstream: ' . intval($arr['upstream']));
 //	logger('notifier_array: ' . print_r($arr,true), LOGGER_ALL, LOG_INFO);
 
-	if($arr['target_item']['item_rss']) {
-		return;
-	}
-
-	if($arr['parent_item']['item_rss']) {
-		return;
-	}
-
 	// allow this to be set per message
 
 	if(($arr['mail']) && intval($arr['item']['raw'])) {
