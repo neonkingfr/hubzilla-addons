6120578a9eb3a8b1f3d0aa2178e1c6ecec7837c8
Mario <mario@mariovavti.com>
2021-05-27 11:13:13 +0000

    remove include zot

--- pubcrawl/Mod_Ap_probe.php
+++ pubcrawl/Mod_Ap_probe.php
@@ -3,22 +3,21 @@ namespace Zotlabs\Module;
 
 use Zotlabs\Web\HTTPSig;
 
-require_once('include/zot.php');
 require_once('library/jsonld/jsonld.php');
 
 class Ap_probe extends \Zotlabs\Web\Controller {
 
 	function get() {
-	
+
 		$o .= '<h3>ActivityPub Probe Diagnostic</h3>';
-	
+
 		$o .= '<form action="ap_probe" method="post">';
 		$o .= 'Lookup URI: <input type="text" style="width: 250px;" name="addr" value="' . $_REQUEST['addr'] .'" /><br>';
 		$o .= 'or paste text: <textarea style="width: 250px;" name="text">' . htmlspecialchars($_REQUEST['text']) . '</textarea><br>';
-		$o .= '<input type="submit" name="submit" value="Submit" /></form>'; 
-	
+		$o .= '<input type="submit" name="submit" value="Submit" /></form>';
+
 		$o .= '<br /><br />';
-	
+
 		if(x($_REQUEST,'addr')) {
 			$addr = $_REQUEST['addr'];
 
@@ -33,7 +32,7 @@ class Ap_probe extends \Zotlabs\Web\Controller {
 
 
 				$o .= '<pre>' . htmlspecialchars($x['body']) . '</pre>' . EOL;
-				
+
 				$o .= 'verify returns: ' . str_replace("\n",EOL,print_r(HTTPSig::verify($x),true)) . EOL;
 				$text = $x['body'];
 			}
@@ -45,18 +44,18 @@ class Ap_probe extends \Zotlabs\Web\Controller {
 
 //				if($text && json_decode($text)) {
 //					$normalized1 = jsonld_normalize(json_decode($text),[ 'algorithm' => 'URDNA2015', 'format' => 'application/nquads' ]);
-//					$o .= str_replace("\n",EOL,htmlentities(var_export($normalized1,true))); 
+//					$o .= str_replace("\n",EOL,htmlentities(var_export($normalized1,true)));
 
 	//				$o .= '<pre>' . json_encode($normalized1, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . '</pre>';
 //				}
 
 				$o .= '<pre>' . str_replace(['\\n','\\'],["\n",''],htmlspecialchars(jindent($text))) . '</pre>';
 
-				$AP = new \Zotlabs\Lib\ActivityStreams($text);	
+				$AP = new \Zotlabs\Lib\ActivityStreams($text);
 				$o .= '<pre>' . htmlspecialchars($AP->debug()) . '</pre>';
 		}
 
 		return $o;
 	}
-	
+
 }
