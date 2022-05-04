e08a415c803c7491b6e07b2ef0b2510307ddbde7
Mario <mario@mariovavti.com>
2022-05-04 18:48:24 +0000

    move cards to addon

---
+++ cards/cards.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Name: Cards
+ * Description: Create interactive personal planning cards
+ * Version: 1.0
+ */
+
+use Zotlabs\Lib\Apps;
+use Zotlabs\Extend\Hook;
+use Zotlabs\Module\Card_edit;
+
+require_once('addon/cards/Mod_Cards.php');
+
+function cards_load() {
+	Hook::register('module_loaded', 'addon/cards/cards.php', 'cards_load_module');
+}
+
+function cards_unload() {
+	Hook::unregister('module_loaded', 'addon/gallery/cards.php', 'cards_load_module');
+}
+
+function cards_load_module(&$b) {
+	if ($b['module'] === 'card_edit') {
+		require_once('addon/cards/Mod_Card_edit.php');
+		$b['controller'] = new Card_edit();
+		$b['installed']  = true;
+	}
+}
